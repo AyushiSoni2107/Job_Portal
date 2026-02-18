ec357a378f7c239d977961ab8232f64104389870
AyushiSoni2107 <ayushisoni2102@gmail.com>
2026-02-18 17:44:20 +0530

    first commit

---
+++ backend/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    avatar: String,
+    role: { type: String, enum: ["jobseeker", "employer"], required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    resume: String,
+
+    // for employer
+    companyName: String,
+    companyDescription: String,
+    companyLogo: String,
+  },
+  { timestamps: true }
+);
+
+// Encrypt password before save
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// Match entered password
+userSchema.methods.matchPassword = function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
